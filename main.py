c0dbd4fb843e1ad6aeee48b5d1bd3602acdee99d
burgerhex <aviv.shai@gmail.com>
2021-12-28 00:44:25 -1000

    Add login stuff

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import time
+import pickle
+from getpass import getpass
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+LOGIN_FILE = "login.pkl"
 
+try:
+    with open(LOGIN_FILE, "rb") as file:
+        user, password = pickle.load(file)
+    response = input(f"Use saved user {user}? [Y/n]: ")
+    if response.lower() in ["n", "no"]:
+        raise FileNotFoundError()
+except FileNotFoundError:
+    user = input("Username: ")
+    password = getpass("Password: ")
+    save = input("Remember this login? [y/N]: ")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if save.lower() in ["yes", "y"]:
+        with open(LOGIN_FILE, "wb") as file:
+            pickle.dump((user, password), file)
+        print("Saved login info!")
 
+BASE_URL = "https://www.wizard101.com/quiz/trivia/game/"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+service = Service(r"C:\Program Files\ChromeDriver\chromedriver.exe")
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+driver = webdriver.Chrome(service=service, options=options)
+driver.maximize_window()
+driver.get("https://www.wizard101.com/game")
+# time.sleep(1)
+user_box = driver.find_element(By.ID, "loginUserName")
+user_box.clear()
+user_box.send_keys(user)
+password_box = driver.find_element(By.ID, "loginPassword")
+password_box.clear()
+password_box.send_keys(password)
+submit_button = driver.find_element(By.CSS_SELECTOR, "input[type=submit]")
+# time.sleep(2)
+submit_button.click()
+assert "You might have misspelled something" not in driver.page_source
+# time.sleep(5)
+# driver.close()
